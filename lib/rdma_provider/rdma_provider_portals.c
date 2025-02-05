5af29b14e6ee3d0c677eb5fa4b8313d20ec6a41b
Giorgos Saloustros <gesalous@ics.forth.gr>
2025-02-05 11:27:42 +0200

    [WIP][feat] Dummy implementation of PORTALS provider

--- lib/rdma_provider/rdma_provider_portals.c
+++ lib/rdma_provider/rdma_provider_portals.c
@@ -14,11 +14,85 @@
 #include "spdk_internal/rdma_utils.h"
 #include "spdk/log.h"
 
+#define UNIMPLEMENTED() do { \
+    fprintf(stderr, "UNIMPLEMENTED %s:%s:%d\n", __FILE__, __func__, __LINE__); \
+    exit(EXIT_FAILURE); \
+} while(0);
+//from common shit
+
+struct spdk_rdma_provider_srq *
+spdk_rdma_provider_srq_create(struct spdk_rdma_provider_srq_init_attr *init_attr)
+{
+	assert(init_attr);
+	assert(init_attr->pd);
+  UNIMPLEMENTED()
+	return NULL;
+}
+
+int
+spdk_rdma_provider_srq_destroy(struct spdk_rdma_provider_srq *rdma_srq)
+{
+
+	if (!rdma_srq) {
+		return 0;
+	}
+
+	assert(rdma_srq->srq);
+  UNIMPLEMENTED()
+	free(rdma_srq);
+  return -1;
+}
+
+static inline bool
+rdma_queue_recv_wrs(struct spdk_rdma_provider_recv_wr_list *recv_wrs, struct ibv_recv_wr *first,
+		    struct spdk_rdma_provider_wr_stats *recv_stats)
+{
+  UNIMPLEMENTED()
+  return false;
+}
+
+bool
+spdk_rdma_provider_srq_queue_recv_wrs(struct spdk_rdma_provider_srq *rdma_srq,
+				      struct ibv_recv_wr *first)
+{
+	assert(rdma_srq);
+	assert(first);
+  UNIMPLEMENTED()
+  return false;
+}
+
+int
+spdk_rdma_provider_srq_flush_recv_wrs(struct spdk_rdma_provider_srq *rdma_srq,
+				      struct ibv_recv_wr **bad_wr)
+{
+  UNIMPLEMENTED()
+  return -1;
+}
+
+bool
+spdk_rdma_provider_qp_queue_recv_wrs(struct spdk_rdma_provider_qp *spdk_rdma_qp,
+				     struct ibv_recv_wr *first)
+{
+	assert(spdk_rdma_qp);
+	assert(first);
+  UNIMPLEMENTED()
+  return false;
+}
+
+int
+spdk_rdma_provider_qp_flush_recv_wrs(struct spdk_rdma_provider_qp *spdk_rdma_qp,
+				     struct ibv_recv_wr **bad_wr)
+{
+  UNIMPLEMENTED()
+	return -1;
+}
+// common end
+
 struct spdk_rdma_provider_qp *
 spdk_rdma_provider_qp_create(struct rdma_cm_id *cm_id,
 			     struct spdk_rdma_provider_qp_init_attr *qp_attr)
 {
-  fprintf(stderr,"%s:%s:%d\n",__FILE__,__func__,__LINE__);
+  UNIMPLEMENTED()
   return NULL;
 }
 
@@ -28,8 +102,7 @@ spdk_rdma_provider_qp_accept(struct spdk_rdma_provider_qp *spdk_rdma_qp,
 {
 	assert(spdk_rdma_qp != NULL);
 	assert(spdk_rdma_qp->cm_id != NULL);
-
-  fprintf(stderr,"%s:%s:%d\n",__FILE__,__func__,__LINE__);
+  UNIMPLEMENTED()
 	return 0;
 }
 
@@ -37,7 +110,7 @@ int
 spdk_rdma_provider_qp_complete_connect(struct spdk_rdma_provider_qp *spdk_rdma_qp)
 {
 	/* Nothing to be done for Verbs */
-  fprintf(stderr,"%s:%s:%d\n",__FILE__,__func__,__LINE__);
+  UNIMPLEMENTED()
 	return 0;
 }
 
@@ -45,18 +118,15 @@ void
 spdk_rdma_provider_qp_destroy(struct spdk_rdma_provider_qp *spdk_rdma_qp)
 {
 	assert(spdk_rdma_qp != NULL);
-
-  fprintf(stderr,"%s:%s:%d\n",__FILE__,__func__,__LINE__);
-
+  UNIMPLEMENTED()
 	free(spdk_rdma_qp);
 }
 
 int
 spdk_rdma_provider_qp_disconnect(struct spdk_rdma_provider_qp *spdk_rdma_qp)
 {
-
 	assert(spdk_rdma_qp != NULL);
-  fprintf(stderr,"%s:%s:%d\n",__FILE__,__func__,__LINE__);
+  UNIMPLEMENTED()
   return 0;
 }
 
@@ -67,6 +137,7 @@ spdk_rdma_provider_qp_queue_send_wrs(struct spdk_rdma_provider_qp *spdk_rdma_qp,
 
 	assert(spdk_rdma_qp);
 	assert(first);
+  UNIMPLEMENTED()
   return false;
 }
 
@@ -74,13 +145,14 @@ int
 spdk_rdma_provider_qp_flush_send_wrs(struct spdk_rdma_provider_qp *spdk_rdma_qp,
 				     struct ibv_send_wr **bad_wr)
 {
-  fprintf(stderr,"%s:%s:%d\n",__FILE__,__func__,__LINE__);
+  UNIMPLEMENTED()
   return 0;
 }
 
 bool
 spdk_rdma_provider_accel_sequence_supported(void)
 {
-  fprintf(stderr,"%s:%s:%d\n",__FILE__,__func__,__LINE__);
+  UNIMPLEMENTED()
 	return false;
 }
+
