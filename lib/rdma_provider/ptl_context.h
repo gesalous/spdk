41f488afabe25946a21d600c7917eb8b8c59f19d
Giorgos Saloustros <gesalous@ics.forth.gr>
2025-02-14 10:47:29 +0200

    [WIP][feat] Add create srq function for SPDK PORTALS

--- lib/rdma_provider/ptl_context.h
+++ lib/rdma_provider/ptl_context.h
@@ -1,6 +1,7 @@
 #ifndef PTL_CONTEXT_H
 #define PTL_CONTEXT_H
 #include "portals4.h"
+#include <stdbool.h>
 struct ptl_context;
 struct ibv_context;
 struct ibv_pd;
@@ -14,4 +15,6 @@ struct ptl_context *ptl_cnxt_get_from_ibvpd(struct ibv_pd *ib_pd);
 ptl_handle_eq_t ptl_cnxt_get_event_queue(struct ptl_context *cnxt);
 ptl_pt_index_t ptl_cnxt_get_portal_index(struct ptl_context *cnxt);
 ptl_handle_ni_t ptl_cnxt_get_ni_handle(struct ptl_context *cnxt);
+bool ptl_cnxt_add_md(struct ptl_context *cnxt, void *vaddr, size_t size,
+                     ptl_handle_md_t memory_handle);
 #endif
